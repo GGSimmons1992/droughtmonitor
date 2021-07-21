a3880bd4eb7b3eefbed0e47c01261ed27b39717e
Gary Simmons <gary.simmons@colorado.edu>
2021-07-21 00:14:17 -0500

    create bootstrap plot

--- src/dataProcessor.py
+++ src/dataProcessor.py
@@ -16,7 +16,8 @@ def defineColor(dcat):
     if (dcat == 4):
         return 'black'
 
-def plotConsecTimeSeries(ax,df,countyName,legendLoc):
+def plotConsecTimeSeries(df,countyName,legendLoc):
+    fig,ax = plt.subplots()
     for index, row in df.iterrows():
         x=[row['StartDate'],row['EndDate']]
         y=[row['DCat'],row['DCat']]
@@ -34,43 +35,27 @@ def plotConsecTimeSeries(ax,df,countyName,legendLoc):
             handle_list.append(handle)
             label_list.append(label)
     ax.legend(handle_list, label_list,loc=legendLoc)
-    plt.show()
     plt.savefig(f'../images/{countyName}.png')
+    plt.show()
 
-if __name__ == "__main__":
-    df0 = pd.read_csv("../data/CA2020/ca2020D0.csv").set_index('County')
-    df1 = pd.read_csv("../data/CA2020/ca2020D1.csv").set_index('County')
-    df2 = pd.read_csv("../data/CA2020/ca2020D2.csv").set_index('County')
-    df3 = pd.read_csv("../data/CA2020/ca2020D3.csv").set_index('County')
-    df4 = pd.read_csv("../data/CA2020/ca2020D4.csv").set_index('County')
+def bootstrap_means(arr):
+    data = np.array(arr)
+    sampleMeans = []
+    for row in range(10000):
+        bootstrappedSample = np.array([np.random.choice(data) for _ in range(len(data))])
+        sampleMeans.append(bootstrappedSample.mean())
+    meanOfSamples = np.mean(sampleMeans)
+    lowerCI,upperCI = np.percentile(sampleMeans,[2.5,97.5])
+    return sampleMeans,meanOfSamples,lowerCI,upperCI
 
-    df0 = df0.rename(columns = {"NonConsecutiveWeeks":"d0"})
-    df1 = df1.rename(columns = {"NonConsecutiveWeeks":"d1"})
-    df2 = df2.rename(columns = {"NonConsecutiveWeeks":"d2"})
-    df3 = df3.rename(columns = {"NonConsecutiveWeeks":"d3"})
-    df4 = df4.rename(columns = {"NonConsecutiveWeeks":"d4"})
-    
-    california2020 = pd.DataFrame(data={"FIPS":df0["FIPS"]})
-    california2020['d4'] = df4['d4']
-    california2020 = california2020.replace(np.nan,0)
-    california2020['d3'] = df3['d3'] - california2020['d4']
-    california2020 = california2020.replace(np.nan,0)
-    california2020['d2'] = df2['d2'] - california2020['d3'] - california2020['d4']
-    california2020 = california2020.replace(np.nan,0)
-    california2020['d1'] = df1['d1'] - california2020['d2'] - california2020['d3'] - california2020['d4']
-    california2020 = california2020.replace(np.nan,0)
-    california2020['d0'] = df0['d0'] - california2020['d1']- california2020['d2'] - california2020['d3'] - california2020['d4']
-    california2020 = california2020.replace(np.nan,0)
-
-    consec0 = pd.read_csv("../data/CA2020/d0Consecutive.csv")
-    consec1 = pd.read_csv("../data/CA2020/d1Consecutive.csv")
-    consec2 = pd.read_csv("../data/CA2020/d2Consecutive.csv")
-    consec3 = pd.read_csv("../data/CA2020/d3Consecutive.csv")
-    consec4 = pd.read_csv("../data/CA2020/d4Consecutive.csv")
-
-    consec0['DCat']=0
-    consec1['DCat']=1
-    consec2['DCat']=2
-    consec3['DCat']=3
-    consec4['DCat']=4
-    consecCA2020 = pd.concat([consec0,consec1,consec2,consec3,consec4]).sort_values(by=["County","StartDate"])
+def plotBootstrapSamples(countyName,sampleMeans,meanOfSamples,lowerCI,upperCI):
+    fig,ax = plt.subplots()
+    bars = ax.hist(sampleMeans,bins=100)
+    barheight = np.max(bars[0])
+    ax.plot([lowerCI,lowerCI],[0,barheight])
+    ax.plot([meanOfSamples,meanOfSamples],[0,barheight])
+    ax.plot([upperCI,upperCI],[0,barheight])
+    ax.set_xlabel('average category')
+    ax.set_title(f'{countyName} avg category = {meanOfSamples} ([{lowerCI},{upperCI}])')
+    plt.savefig(f'../images/{countyName}95percentCI.png')
+    plt.show()
